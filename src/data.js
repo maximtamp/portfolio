0a862da8c328e7a545543cc45dba4a9e9f4ee419
maximtamp <maxim.tampere@gmail.com>
2025-08-21 21:11:58 +0200

    Nav better, CSS Skills mostly done and skills data added

---
+++ src/data.js
@@ -0,0 +1,54 @@
+export const skills = [
+        {
+            title: "development",
+            tools: [
+                { name: "VS Code", icon: "vs-code-icon.png" },
+                { name: "HTML", icon: "html-icon.png" },
+                { name: "CSS", icon: "css-icon.png" },
+                { name: "JavaScript", icon: "js-icon.png" },
+                { name: "Astro", icon: "astro-icon.png" },
+                { name: "php", icon: "php-icon.png" },
+                { name: "Vite", icon: "vite-icon.png" },
+                { name: "React", icon: "react-icon.png" },
+                { name: "React Router", icon: "react-router-icon.png" },
+                { name: "Strapi", icon: "strapi-icon.png" },
+                { name: "Arduino", icon: "arduino-icon.png" },
+                { name: "ml5", icon: "ml5-icon.png" },
+                { name: "GSAP", icon: "gsap-icon.png" },
+                { name: "Github", icon: "github-icon.png" },
+            ],
+        skills: ["Responsive", ".json", "Database", "Terminal", "API", "Git", "Deployment", "json Server", "Arduino", "Arduino Ethernet"]
+        },
+        {
+            title: "ux",
+            tools: [
+                { name: "Miro", icon: "miro-icon.png" },
+                { name: "Figjam", icon: "figjam-icon.png" },
+                { name: "Google Form", icon: "google-form-icon.png" },
+            ],
+            skills: ["User Testing", "Interview", "Data Visualization", "Information Architecture", "Design Principles", "Prototype", "HMW",
+                "Buy the Feature", "Customer Journey", "Wireframe", "Cultural Probe", "Desk Research", "Persona",
+                "User Group Definition", "Archetype", "Crazy8's", "Mindmap", "Scamper", "Random Word Associations", "COCD-Box",
+                "6 Thinking Hats", "Decision Matrix", "Assumption Matrix", "User Flow", "Flowchart", "Task Flow", "Wire Flow",
+                "User Goals", "Levels Of Expertise", "Dubble Diamond", "Storyboard", "MoSCoW", "Annotation", "User Tasks",
+                "Iteratie", "Survey", "Observation", "10 Heuristics", "Sketchnotes"]
+        },
+        {
+            title: "design",
+            tools: [
+                { name: "Figma", icon: "figma-icon.png" },
+                { name: "Illustrator", icon: "illustrator-icon.png" },
+                { name: "Photoshop", icon: "photoshop-icon.png" },
+            ],
+            skills: ["Responsive", "Logo Design", "Branding", "Icon Design", "Styleboard", "Moodboard", "App Design", "Storytelling", "Digital Campaign", "Typography", "History Of Art/Design"]
+        },
+        {
+            title: "motion",
+            tools: [
+                { name: "Adobe After Effects", icon: "after-effects-icon.png" },
+                { name: "Adobe Media Encoder", icon: "media-encoder-icon.png" },
+                { name: "Adobe Audition", icon: "audition-icon.png" },
+            ],
+            skills: ["Animation", "Animation Principles", "Product Animation", "Pitch Deck", "Animatic", "Rough Edit", "Sound Design"]
+        },
+];
